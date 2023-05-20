d4546b3547f9d3de815cea3f640790fe3118cbff
Muhie Aldeen <zezo1998@hotmail.com>
2023-05-20 17:06:54 +0200

    I wrote a function to get the latitude and logitude to pas them in the maine url to get the weather info for the chosen city

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cityName = submitButton.addEventListener('click', () => {
   handleCityName(inputValue);
 });
 
-function handleCityName(cityName) {
+async function handleCityName(cityName) {
   console.log(cityName);
   const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=en&format=json`;
 
@@ -24,28 +24,29 @@ function handleCityName(cityName) {
       console.log(error);
     }
   }
-  getCityLocationData(geoUrl)
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+  const data = await getCityLocationData(geoUrl);
 }
+const latitude = data.results[0].latitude;
+const longitude = data.results[0].longitude;
+console.log('Latitude:', latitude);
+console.log('Longitude:', longitude);
 
-//   //-------------------------- In this part of code we fething the weather data for the chosen city --------------------------
+const mainUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum&current_weather=true&forecast_days=16&timezone=auto`;
 
-// const mainUrl =
-//   'https://api.open-meteo.com/v1/forecast?latitude=52.37&longitude=4.89&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum&current_weather=true&forecast_days=16&timezone=auto';
+//-------------------------- In this part of code we fething the weather data for the chosen city --------------------------
 
-// async function getData(url) {
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Network error:${response.status}`);
-//     }
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// getData(mainUrl)
-//   .then((data) => console.log(data))
-//   .catch((error) => console.error(error));
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Network error:${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(mainUrl)
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));

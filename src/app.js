f08510975be406650648c562b24e9bfbcefb4a92
Muhie Aldeen <zezo1998@hotmail.com>
2023-05-20 22:06:12 +0200

    I modified the name of the variable citName to be cityNameAndNumberOFdays and adjust the function in the eventListener to get the name of the city and the number of days

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-//----------------------- In this part of code we fething the goecdes to get the latitude and longitude of the chosen city -----------------------------
-
 const searchInput = document.querySelector('.search');
 const submitButton = document.querySelector('.submit-button');
 
-const cityName = submitButton.addEventListener('click', () => {
-  const inputValue = searchInput.value.toLowerCase();
-  handleCityName(inputValue);
+const cityNameAndNumberOFdays = submitButton.addEventListener('click', () => {
+  const writtenCityName = searchInput.value.toLowerCase();
+
+  const selectElement = document.querySelector('.select-day');
+  const selectedValue = selectElement.value;
+
+  console.log(selectedValue);
+  handleCityName(writtenCityName, selectedValue);
 });
 
-async function handleCityName(cityName) {
+async function handleCityName(cityName, numberOfDays) {
   console.log(cityName);
   const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=1&language=en&format=json`;
 
@@ -22,19 +25,24 @@ async function handleCityName(cityName) {
       return data;
     } catch (error) {
       console.log(error);
+      throw error;
     }
   }
-  const data = await getCityLocationData(geoUrl);
+
+  try {
+    const data = await getCityLocationData(geoUrl);
+    const latitude = data.results[0].latitude;
+    const longitude = data.results[0].longitude;
+    console.log('Latitude:', latitude);
+    console.log('Longitude:', longitude);
+    const mainUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum&current_weather=true&forecast_days=${numberOfDays}&timezone=auto`;
+
+    await getData(mainUrl);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
-const latitude = data.results[0].latitude;
-const longitude = data.results[0].longitude;
-console.log('Latitude:', latitude);
-console.log('Longitude:', longitude);
-
-const mainUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum&current_weather=true&forecast_days=16&timezone=auto`;
-
-//-------------------------- In this part of code we fething the weather data for the chosen city --------------------------
-
 async function getData(url) {
   try {
     const response = await fetch(url);
@@ -42,11 +50,10 @@ async function getData(url) {
       throw new Error(`Network error:${response.status}`);
     }
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.log(error);
+    throw error;
   }
 }
-getData(mainUrl)
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
